325caff80769b572c4fe938e817c71be5461b8df
Roman <42904990+anufrievroman@users.noreply.github.com>
2020-05-28 13:12:32 +0900

    Update to python 3

--- callaway_holland_model.py
+++ callaway_holland_model.py
@@ -1,15 +1,17 @@
-# This python code is written by Dr. Roman Anufriev for Nomura lab, IIS, University of Tokyo in 2018.
+# This python code is written by Dr. Roman Anufriev for Nomura lab, IIS, University of Tokyo in 2018-2019
 # The code implements Callaway-Holland model which calculates the thermal conductivity of nanostructures.
 # Contact me by anufriev.roman@protonmail.com if you have any questions.
 
-from numpy import pi, exp, zeros, savetxt, sqrt
+from numpy import pi, exp, zeros, savetxt, sqrt, sinh
+#import matplotlib
+#matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from scipy.constants import k, hbar
 
 
 def parameters():
-    #T=300 
-    N=2000
+    '''Here we define main parameters of the calculation'''
+    N=1000
     roughness=2.0e-9
     return N, roughness
 
@@ -37,6 +39,7 @@ def phonon_properties_assignment_2(j, branch, N, dispersion):
 
 
 def relaxation_times(K, w, L_parameter, speed, T, p, polarization):
+    '''This function calculates relaxation times and summs them according to the Mathiesen's rule'''
     A=2.95e-45                                                              # Parameters from bulk fitting                                                            
     B=0.95e-19                                                              
     #E=3.3e-3								    # Crystal boundary 
@@ -49,21 +52,28 @@ def relaxation_times(K, w, L_parameter, speed, T, p, polarization):
     #    time_u=((3.28e-19)*(w**2)*T*exp(-140/T))  
     time_h = (speed/L_parameter)*(1.0-p)/(1.0+p)                            # scattering in the necks
     time_relaxation = 1.0/(time_i+time_u+time_h)                            # Total relaxation time
-    time_relaxation = 1.0/(time_i+time_h)				    # Total relaxation time
     return time_relaxation
 
 
 def main():
     '''This is the main function, which calculates the thermal conductivity'''
+    material_density=2330 #[kg/m^3]
+
     N, roughness=parameters()
     dispersion=bulk_phonon_dispersion(N+1)
-    temperatures=range(2, 304, 2)
+    temperatures=range(5, 305, 5)
+    #Lc=[70e-9, 100e-9, 150e-9, 200e-9, 250e-9, 300e-9, 350e-9, 400e-9, 500e-9]
     thermal_conductivities=zeros((len(temperatures),2))
-    L_parameter=1.12*sqrt((145e-9)*(150e-9))                                # This is the limiting dimension, i.e. neck of PnC or NW width etc.
-    #L_parameter=100e-9
+    specific_heat_capacities=zeros((len(temperatures),2))
+    #mfp=zeros((N,5))
+    #L_parameter=1.12*sqrt((145e-9)*(150e-9))                                # This is the limiting dimension, i.e. neck of PnC or NW width etc.
+    L_parameter=sqrt(2)*165e-9
     print("Please wait...")
+    T=300 
     for index, T in enumerate(temperatures):
+    #for index, L_parameter in enumerate(Lc):
         thermal_conductivity=0.0
+        total_heat_capacity=0.0
         T=float(T)
         for branch in range(3):                                             # For each phonon branch
             for j in range(N):    
@@ -71,20 +81,39 @@ def main():
                 p=exp(-4.0*(K**2.0)*roughness**2.0)                         # Specularity. Ref. APL 106, 133108 (2015)
                 time_relaxation=relaxation_times(K,w,L_parameter,speed,T,p,polarization)                  
                 heat_capacity=k*((hbar*w/(k*T))**2.0)*exp(hbar*w/(k*T))/((exp(hbar*w/(k*T))-1.0)**2.0) # Ref. PRB 88 155318 (2013)
-                thermal_conductivity+=(1.0/(6.0*(pi**2.0)))*heat_capacity*(speed**2.0)*time_relaxation*(K**2.0)*dK          
+                thermal_conductivity+=(1.0/(6.0*(pi**2.0)))*heat_capacity*(speed**2.0)*time_relaxation*K**2.0*dK          
+                total_heat_capacity += (1/(2*pi**2))*heat_capacity*K**2.0*dK    #[J/K] Total heat capacity integrated over all branches and wavevectors
+#		mfp[j,0]=(w/(2*pi))*1e-12
+#		mfp[j,branch+1]=speed*time_relaxation 
+
         thermal_conductivities[index,0]=T
         thermal_conductivities[index,1]=thermal_conductivity
+        specific_heat_capacities[index,0]=T
+        specific_heat_capacities[index,1]=total_heat_capacity/material_density # [J/kg/K]
+
     plt.plot(thermal_conductivities[:,0],thermal_conductivities[:,1])
     plt.ylabel('Thermal conductivity (W/mK)', fontsize=12)
     plt.xlabel('Temperature (K)', fontsize=12)
     plt.show()
-    savetxt('Callaway_Holland_model_results.txt', thermal_conductivities, delimiter="	")
+    savetxt('thermal_conductivity.txt', thermal_conductivities, delimiter="	")
 
-    #plt.plot(dispersion[:,0],dispersion[:,1],dispersion[:,0],dispersion[:,2],dispersion[:,0],dispersion[:,3])
-    #plt.ylabel('W', fontsize=12)
-    #plt.xlabel('K', fontsize=12)
-    #plt.show()
+#    plt.loglog(mfp[:,0],mfp[:,1],mfp[:,0],mfp[:,2],mfp[:,0],mfp[:,3])
+#    plt.ylabel('Mean Free Path (m)', fontsize=12)
+#    plt.xlabel('Frequency (THz)', fontsize=12)
+#    plt.show()
+    
+#    plt.plot(specific_heat_capacities[:,0], specific_heat_capacities[:,1])
+#    plt.ylabel('Heat capacity (J/kg/K)', fontsize=12)
+#    plt.xlabel('Temperature (K)', fontsize=12)
+#    plt.show()
+
+#    plt.plot(dispersion[:,0],dispersion[:,1],dispersion[:,0],dispersion[:,2],dispersion[:,0],dispersion[:,3])
+#    plt.ylabel('Frequency (Hz)', fontsize=12)
+#    plt.xlabel('Wavevector (1/m)', fontsize=12)
+#    plt.show()
+
     return
+
 
 if __name__ == "__main__":
     main()
